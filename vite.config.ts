8a27b29ae55ca5bb69eba05cf156d7b712ae77ef
Flupinochan <flupino@metalmental.net>
2026-03-01 17:15:42 +0900

    gzip, brotliの圧縮配信設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,36 @@
 import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
 import vuetify from "vite-plugin-vuetify";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+    visualizer({
+      template: "treemap",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: "./tmp/stats.html",
+    }),
+    // amplify.ymlのContent-Encodingヘッダーと合わせる
+    viteCompression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      deleteOriginFile: false,
+      threshold: 1024,
+    }),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+      deleteOriginFile: false,
+      threshold: 1024,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
